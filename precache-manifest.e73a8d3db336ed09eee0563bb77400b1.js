f955fdf8f028d6f18c446d8b3c676e8374b6d332
Amay Kataria <studio@amaykataria.com>
2022-02-21 12:53:38 -0600

    Updates

---
+++ precache-manifest.e73a8d3db336ed09eee0563bb77400b1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b2f1242ae3da13ed4c662956f52e44",
+    "url": "/supersynthesis/index.html"
+  },
+  {
+    "revision": "f666160a4f218f2c1e97",
+    "url": "/supersynthesis/static/css/main.06e97ea4.chunk.css"
+  },
+  {
+    "revision": "d16c86b66a0b9ffaf478",
+    "url": "/supersynthesis/static/js/2.2a01df87.chunk.js"
+  },
+  {
+    "revision": "049cfa4123a65ac5476133f6a18871c6",
+    "url": "/supersynthesis/static/js/2.2a01df87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f666160a4f218f2c1e97",
+    "url": "/supersynthesis/static/js/main.e1514336.chunk.js"
+  },
+  {
+    "revision": "cf8e2b6dbc01e795a660",
+    "url": "/supersynthesis/static/js/runtime-main.522b51db.js"
+  },
+  {
+    "revision": "ea00a3b068be718fb38da53887261d15",
+    "url": "/supersynthesis/static/media/about.ea00a3b0.svg"
+  },
+  {
+    "revision": "3119a454c011d2b2c1f377983a2af134",
+    "url": "/supersynthesis/static/media/airwaves-webfont.3119a454.woff"
+  },
+  {
+    "revision": "97105c1a047d035710b995e1cec235b1",
+    "url": "/supersynthesis/static/media/airwaves-webfont.97105c1a.woff2"
+  },
+  {
+    "revision": "29c20064259c8df60d9830762601bfa3",
+    "url": "/supersynthesis/static/media/josefinsans-semibold-webfont.29c20064.woff"
+  },
+  {
+    "revision": "f1b2a5a6c295521afef328f2c329ff34",
+    "url": "/supersynthesis/static/media/josefinsans-semibold-webfont.f1b2a5a6.woff2"
+  }
+]);

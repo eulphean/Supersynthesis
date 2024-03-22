acdf6ee10072c768234bc7f3ae42cd8a6354d7a7
Amay Kataria <amay.kataria@gmail.com>
2024-03-22 14:32:19 -0500

    Pending changes

--- WebClient/src/components/Websocket.js
+++ WebClient/src/components/Websocket.js
@@ -15,7 +15,7 @@ import SequencerStore from '../stores/SequencerStore';
 import ModeStore from '../stores/ModeStore';
 import SynthStore from '../stores/SynthStore';
 
-//const webURL = "http://localhost:5000";
+//const webURL = "http://localhost:5000";//Comment
 const webURL = "https://supersynth.herokuapp.com";
 
 const EVENT_SCORE_PAYLOAD = 'event_score_payload';
